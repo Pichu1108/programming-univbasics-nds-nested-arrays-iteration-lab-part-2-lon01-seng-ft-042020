0a02b25f48f4d512a85885a97fed56816cb0e1bd
Manuel Alb Martin jukisz <manujukisz@outlook.com>
2020-04-10 16:35:47 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,6 +8,7 @@ def find_min_in_nested_arrays(src)
   while row_index < src.count do
     element_index = 0
     lw_num = 100
+    inner_results = []
     while element_index < src[row_index].count do
         if lw_num > src[row_index][element_index]
           lw_num = src[row_index][element_index]
